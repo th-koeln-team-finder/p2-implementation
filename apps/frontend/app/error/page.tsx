95068f812a5290b1277f5a16927b62e0c9302581
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-12-09 10:24:32 +0100

    feat(#5): add error page

---
+++ apps/frontend/app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function ErrorPage() {
+  return (
+    <main className="container mx-auto my-4">
+      <h1 className="font-bold text-3xl">Oops something went wrong</h1>
+    </main>
+  )
+}
