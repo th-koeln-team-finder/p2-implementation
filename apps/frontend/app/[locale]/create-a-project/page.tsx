3d9db0487124ad504567dd5b2eb3adaab7da1e24
Liobabrandhoff <lb@schrittweiter.de>
2024-12-16 18:04:00 +0100

    added: stepper

--- apps/frontend/app/[locale]/create-a-project/page.tsx
+++ apps/frontend/app/[locale]/create-a-project/page.tsx
@@ -1,7 +1,63 @@
-export default function Brainstorm() {
+import {StepperComponent, ContentItem} from "@repo/design-system/components/stepper";
+import {Input} from "@repo/design-system/components/ui/input";
+import {Label} from "@repo/design-system/components/ui/label";
+
+export default function CreateAProject() {
+
+    const steps = [
+        { id: "basics", title: "Basis", description: "Provide shipping details"},
+        { id: "skills", title: "Fähigkeiten", description: "Review your details"},
+        { id: "team", title: "Team-Mitglieder", description: "Review your details"},
+        { id: "timetable", title: "Zeitplan", description: "Review your details"},
+        { id: "links", title: "Links", description: "Review your details"},
+        { id: "review", title: "Überblick", description: "Review your details"},
+    ];
+
+
   return (
-    <div>
-      <h1>Create a project</h1>
-    </div>
+      <div className='mx-auto w-full max-w-screen-xl p-4'>
+          <StepperComponent steps={steps}>
+
+              <ContentItem stepId="basics">
+                  <div className="flex w-full flex-row gap-4">
+                      <div className="w-1/2">
+                          <Label>Projektname</Label>
+                          <Input placeholder="Type here..."/>
+                      </div>
+                      <div className="w-1/2">
+                          <Label>Projektphase</Label>
+                          <Input placeholder="Type here..."/>
+                      </div>
+                  </div>
+
+                  <div className="flex w-full flex-row gap-4">
+                      <div className="w-1/2">
+                          <Label>Images</Label>
+                          <br/>FileUpload für Images
+                      </div>
+                      <div className="w-1/2">
+                          <Label>Description</Label>
+                          <br/>Textarea
+                      </div>
+                  </div>
+              </ContentItem>
+
+              <ContentItem stepId="skills">
+                  <div>Skill Liste</div>
+              </ContentItem>
+              <ContentItem stepId="team">
+                  <div>Liste an Team Members</div>
+              </ContentItem>
+              <ContentItem stepId="timetable">
+                  <div>Auswahl: Variante Tabelle oder Liste, Eingabe Timetable-Daten</div>
+              </ContentItem>
+              <ContentItem stepId="links">
+                  <div>Issue Link, Location Koordinaten, Links & other</div>
+              </ContentItem>
+              <ContentItem stepId="review">
+                  <div>Übersicht aller Angaben</div>
+              </ContentItem>
+           </StepperComponent>
+      </div>
   )
 }
---
+++ packages/design-system/components/stepper.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import { Button } from "../components/ui/button";
+import * as React from "react";
+
+const StepperComponent = ({ steps, children  }: {
+    steps: { id: string; title: string; description?: string }[];
+    children?: React.ReactNode
+}) => {
+    const [currentIndex, setCurrentIndex] = React.useState(0);
+
+    const goToStep = (index: number) => {
+        if (index >= 0 && index < steps.length) {
+            setCurrentIndex(index);
+        }
+    };
+
+    const nextStep = () => goToStep(currentIndex + 1);
+    const prevStep = () => goToStep(currentIndex - 1);
+
+    const reset = () => setCurrentIndex(0);
+
+    const done = () => {
+        //TODO Weiterleitung zu selbst erstellten Projekten?
+    }
+
+    const currentStep = steps[currentIndex];
+
+    // Dynamisch nur das passende ContentItem rendern
+    const stepContent = React.Children.toArray(children).find(
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        (child: any) => child.props?.stepId === currentStep.id
+    );
+
+    return (
+        <div className='flex w-full flex-col items-center'>
+            <div className='mx-auto mb-16 w-fit'>
+                <nav aria-label="Steps Navigation" className="group my-4">
+                    <ol className="flex items-center justify-between gap-2" aria-orientation="horizontal">
+                        {steps.map((step, index) => (
+                            <React.Fragment key={step.id}>
+                                <li className="flex items-center gap-4">
+                                    <Button
+                                        role="tab"
+                                        aria-current={index === currentIndex ? "step" : undefined}
+                                        aria-posinset={index + 1}
+                                        aria-setsize={steps.length}
+                                        className={`flex size-10 items-center justify-center rounded-full ${
+                                            index <= currentIndex ? "bg-primary text-white" : 'border-2 border-fuchsia-700 border-dotted bg-transparent text-fuchsia-700'
+                                        }`}
+                                        onClick={() => goToStep(index)}
+                                    >
+                                        {index + 1}
+                                    </Button>
+                                    <span className={`font-medium text-sm ${index < currentIndex ? "text-primary" : "text-muted-foreground"}`}>{step.title}</span>
+                                </li>
+                                {index < steps.length - 1 &&
+                                    <div className={`-translate-y-1/2 h-2 min-w-20 flex-1 border-b-2 ${index < currentIndex ? "border-fuchsia-700 border-solid" : "border-gray-400 border-dotted"}`}/>}
+                            </React.Fragment>
+                        ))}
+                    </ol>
+                </nav>
+            </div>
+
+            <div className='mb-8 w-full'>
+                <h1 className='mb-4 font-medium text-lg'>{currentStep.title}</h1>
+                {stepContent}
+            </div>
+
+            <div className="mb-8 w-full">
+                <div className="flex justify-center gap-4">
+                    {currentIndex > 0 && (
+                        <Button
+                            className='hover: rounded border-2 border-fuchsia-700 bg-transparent px-4 py-2 text-primary hover:border-fuchsia-800 hover:bg-fuchsia-800 hover:text-white'
+                            onClick={prevStep}
+                        >
+                            Back
+                        </Button>
+                    )}
+                    {currentIndex < steps.length - 1 ? (
+                        <Button
+                            className='rounded px-4 py-2 text-white hover:bg-fuchsia-800'
+                            onClick={nextStep}
+                        >
+                            Next
+                        </Button>
+                    ) : (
+                        <Button
+                            className='rounded bg-gray-400 px-4 py-2 text-white hover:bg-gray-500'
+                            onClick={reset}
+                        >
+                            Reset
+                        </Button>
+                    )}
+                    {currentIndex === steps.length - 1 && (
+                            <Button
+                                className='rounded px-4 py-2 text-white hover:bg-fuchsia-800'
+                                onClick={done}
+                            >
+                                Done
+                            </Button>
+                        )
+                    }
+                </div>
+            </div>
+
+            <div className='mx-auto text-muted-foreground text-sm'>
+                Step {currentIndex + 1} of {steps.length}
+            </div>
+        </div>
+    );
+};
+
+
+const ContentItem = ({stepId, children}: { stepId: string, children?: React.ReactNode }) => {
+    return <div className={`${stepId} flex flex-col gap-4`}>{children}</div>;
+};
+
+
+export { StepperComponent, ContentItem };
+
