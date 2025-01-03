4f44688b4d01cd01166c5ccd3662b735605a5803
gutentag2012 <joshua.gawenda+github@gmail.com>
2025-01-03 22:26:19 +0100

    chore: move out register button

--- apps/frontend/app/[locale]/(unauthenticated)/layout.tsx
+++ apps/frontend/app/[locale]/(unauthenticated)/layout.tsx
@@ -1,8 +1,7 @@
 import { authMiddleware } from '@/auth'
 import { LoginButton } from '@/features/auth/components/LoginButton'
+import { RegisterButton } from '@/features/auth/components/RegisterButton'
 import { UserProfileMenu } from '@/features/auth/components/UserProfileMenu'
-import { Link } from '@/features/i18n/routing'
-import { Button } from '@repo/design-system/components/ui/button'
 import { getTranslations } from 'next-intl/server'
 
 export default async function UnauthenticatedLayout({
@@ -13,7 +12,7 @@ export default async function UnauthenticatedLayout({
   modals: React.ReactNode
 }>) {
   const session = await authMiddleware()
-  const translate = await getTranslations()
+  const _translate = await getTranslations()
   return (
     <>
       <nav className="flex h-16 flex-row justify-end gap-2 bg-card p-4">
@@ -22,9 +21,7 @@ export default async function UnauthenticatedLayout({
         ) : (
           <>
             <LoginButton />
-            <Link href="/register">
-              <Button>{translate('auth.register.button')}</Button>
-            </Link>
+            <RegisterButton />
           </>
         )}
       </nav>
