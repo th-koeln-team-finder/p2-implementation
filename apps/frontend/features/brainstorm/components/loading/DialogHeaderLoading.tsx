b8c6a3ac9475a76fdf9822eb964dc5087becc7f9
gutentag2012 <joshua.gawenda+github@gmail.com>
2025-01-03 22:25:25 +0100

    fix: dialog warning

--- apps/frontend/features/brainstorm/components/loading/DialogHeaderLoading.tsx
+++ apps/frontend/features/brainstorm/components/loading/DialogHeaderLoading.tsx
@@ -1,3 +1,7 @@
+import {
+  DialogHeader,
+  DialogTitle,
+} from '@repo/design-system/components/ui/dialog'
 import { Skeleton } from '@repo/design-system/components/ui/skeleton'
 import { cn } from '@repo/design-system/lib/utils'
 
@@ -9,12 +13,14 @@ export function DialogHeaderLoading({
   withoutIndent,
 }: DialogHeaderLoadingProps) {
   return (
-    <div className={cn('flex flex-col gap-1', !withoutIndent && 'pr-4')}>
-      <div className="flex flex-row items-center justify-between">
+    <DialogHeader
+      className={cn('flex flex-col gap-1', !withoutIndent && 'pr-4')}
+    >
+      <DialogTitle className="flex flex-row items-center justify-between">
         <Skeleton className="h-9 w-1/3 rounded" />
         <Skeleton className="h-9 w-1/4 rounded" />
-      </div>
+      </DialogTitle>
       <Skeleton className="h-4 w-1/4 rounded" />
-    </div>
+    </DialogHeader>
   )
 }
