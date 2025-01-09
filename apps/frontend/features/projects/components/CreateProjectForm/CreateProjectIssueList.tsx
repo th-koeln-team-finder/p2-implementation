2c43d6b6d24ec663c8481f39e4cdd23f16cca009
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-01-09 20:07:29 +0100

    feat: use rich text for description

--- apps/frontend/features/projects/components/CreateProjectForm/CreateProjectIssueList.tsx
+++ apps/frontend/features/projects/components/CreateProjectForm/CreateProjectIssueList.tsx
@@ -1,85 +1,98 @@
 'use client'
 
+import type { CreateProjectFormLinks } from '@/features/projects/projects.types'
 import { useFieldContext } from '@formsignals/form-react'
+import { ZodAdapter } from '@formsignals/validation-adapter-zod'
+import { useSignals } from '@preact/signals-react/runtime'
 import { FieldError } from '@repo/design-system/components/FormErrors'
+import {
+  type LexicalEditorRef,
+  WysiwygEditorForm,
+  getStringContentFromEditor,
+} from '@repo/design-system/components/WysiwygEditor'
 import { Button } from '@repo/design-system/components/ui/button'
 import { InputForm } from '@repo/design-system/components/ui/input'
 import { Label } from '@repo/design-system/components/ui/label'
-import {useSignals} from "@preact/signals-react/runtime";
-// biome-ignore lint/style/useImportType: import type {CreateProjectFormLinks} from "@/features/projects/projects.types";
-import {CreateProjectFormLinks} from "@/features/projects/projects.types";
-import {useLexicalEditorRef, getStringContentFromEditor, WysiwygEditorForm} from "@repo/design-system/components/WysiwygEditor";
-// biome-ignore lint/style/useImportType: zod adapter
-import {ZodAdapter} from "@formsignals/validation-adapter-zod";
-import {z} from "zod";
-import {MinusIcon, PlusIcon} from "lucide-react";
+import { MinusIcon, PlusIcon } from 'lucide-react'
+import { z } from 'zod'
 
-export function CreateProjectIssueList({editorRef}) {
+type CreateProjectIssueListProps = {
+  editorRef: LexicalEditorRef
+}
+
+export function CreateProjectIssueList({
+  editorRef,
+}: CreateProjectIssueListProps) {
   useSignals()
-  const field = useFieldContext<CreateProjectFormLinks, 'issues', typeof ZodAdapter>()
-
-  const content = getStringContentFromEditor(editorRef.current);
-  console.log("Editor Content:", content);
-
+  const field = useFieldContext<
+    CreateProjectFormLinks,
+    'issues',
+    typeof ZodAdapter
+  >()
   return (
-      <>
-        {field.data.value.map((issue, index) => (
-            <div key={issue.key} className="flex flex-row gap-4">
-              <div className="w-2/6">
-                <field.SubFieldProvider name={`${index}.title`} validator={z.string().min(1)}>
-                  <Label>Title</Label>
-                  <InputForm placeholder="Type title..."/>
-                  <FieldError/>
-                </field.SubFieldProvider>
-              </div>
+    <>
+      {field.data.value.map((issue, index) => (
+        <div key={issue.key} className="flex flex-row gap-4">
+          <div className="w-2/6">
+            <field.SubFieldProvider
+              validatorAdapter={ZodAdapter}
+              name={`${index}.title`}
+              validator={z.string().min(1)}
+            >
+              <Label>Title</Label>
+              <InputForm placeholder="Type title..." />
+              <FieldError />
+            </field.SubFieldProvider>
+          </div>
 
-              <div className="flex w-4/6 flex-row justify-between">
-                {/* TODO add wysiwyg editor with error */}
-                {/*<field.SubFieldProvider name={`${index}.description`} validator={() => {
-                  console.log("Editor Content:", getStringContentFromEditor(editorRef.current));
-                  if (!editorRef.current) return null;
-                  return getStringContentFromEditor(editorRef.current).length <= 0 ? "This field is required" : null;
-              }}>
-                  <div>
-                      <Label>Description</Label>
-                      <WysiwygEditorForm editorRef={editorRef}/>
-                      <FieldError/>
-                  </div>
-              </field.SubFieldProvider>*/}
-                <div className="w-5/6 mr-2">
-                  <field.SubFieldProvider name={`${index}.description`} validator={z.string().min(1)}>
-                    <Label>Description</Label>
-                    <InputForm/>
-                    <FieldError/>
-                  </field.SubFieldProvider>
-                </div>
-
-                <div className="flex w-1/6 gap-2">
-                  <Button
-                      onClick={() => field.removeValueFromArray(index)}
-                      variant="outline"
-                      className="mt-auto rounded-full p-2 hover:border-fuchsia-700"
-                  >
-                    <MinusIcon/>
-                  </Button>
-                  <Button
-                      onClick={() => field.pushValueToArray({title: '', description: ''})}
-                      variant="outline"
-                      className="mt-auto rounded-full border-fuchsia-700 p-2 text-fuchsia-700"
-                  >
-                    <PlusIcon/>
-                  </Button>
-                </div>
+          <div className="flex w-4/6 flex-row justify-between">
+            {/* TODO add wysiwyg editor with error */}
+            <field.SubFieldProvider
+              name={`${index}.description`}
+              validator={() => {
+                if (!editorRef.current) return null
+                return getStringContentFromEditor(editorRef.current).length <= 0
+                  ? 'This field is required'
+                  : null
+              }}
+            >
+              <div>
+                <Label>Description</Label>
+                <WysiwygEditorForm editorRef={editorRef} />
+                <FieldError />
               </div>
+            </field.SubFieldProvider>
+
+            <div className="flex w-1/6 gap-2">
+              <Button
+                onClick={() => field.removeValueFromArray(index)}
+                variant="outline"
+                className="mt-auto rounded-full p-2 hover:border-fuchsia-700"
+              >
+                <MinusIcon />
+              </Button>
+              <Button
+                onClick={() =>
+                  field.pushValueToArray({ title: '', description: '' })
+                }
+                variant="outline"
+                className="mt-auto rounded-full border-fuchsia-700 p-2 text-fuchsia-700"
+              >
+                <PlusIcon />
+              </Button>
             </div>
-        ))}
-        {field.data.value.length === 0 && (
-            <Button onClick={() => field.pushValueToArray({title: '', description: ''})} className="my-3"
-                    style={{width: 'fit-content'}}>
-              Add Issue
-            </Button>
-        )}
-
-      </>
+          </div>
+        </div>
+      ))}
+      {field.data.value.length === 0 && (
+        <Button
+          onClick={() => field.pushValueToArray({ title: '', description: '' })}
+          className="my-3"
+          style={{ width: 'fit-content' }}
+        >
+          Add Issue
+        </Button>
+      )}
+    </>
   )
 }
