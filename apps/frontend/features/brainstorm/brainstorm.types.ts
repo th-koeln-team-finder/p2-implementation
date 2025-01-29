9b32f26e5d7a10971684edd9cd91429ce10af29f
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-01-29 18:36:12 +0100

    chore: populate brainstorm resources

--- apps/frontend/features/brainstorm/brainstorm.types.ts
+++ apps/frontend/features/brainstorm/brainstorm.types.ts
@@ -1,12 +1,19 @@
 import type {
   BrainstormCommentSelect,
+  BrainstormResourceSelect,
   BrainstormSelect,
+  UploadedFileSelect,
   UserSelect,
 } from '@repo/database/schema'
+
+export type PopulatedBrainstormResource = BrainstormResourceSelect & {
+  file?: UploadedFileSelect | null
+}
 
 export type PopulatedBrainstorm = BrainstormSelect & {
   isBookmarked: boolean
   tags: { tag: { id: string; name: string } }[]
+  resources: PopulatedBrainstormResource[]
 }
 
 export type PopulatedBrainstormComment = BrainstormCommentSelect & {
@@ -15,4 +22,27 @@ export type PopulatedBrainstormComment = BrainstormCommentSelect & {
   brainstorm?: BrainstormSelect
   creator?: UserSelect
   childComments?: PopulatedBrainstormComment[]
+}
+
+export type CreateBrainstormResourceLink = {
+  type: 'link'
+  label: string
+  value: string
+}
+
+export type CreateBrainstormResourceFile = {
+  type: 'file'
+  label: string
+  fileValue: File[]
+}
+
+export type CreateBrainstormResource =
+  | CreateBrainstormResourceLink
+  | CreateBrainstormResourceFile
+
+export type CreateBrainstormFormValues = {
+  title: string
+  description: string
+  tags: { label: string; value: string }[]
+  resources: CreateBrainstormResource[]
 }
