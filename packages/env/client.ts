2256ce60918d3355e56a3cc2df54ca4b9d3dcdf5
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-01-29 18:32:09 +0100

    chore: move file envs to client env

--- packages/env/client.ts
+++ packages/env/client.ts
@@ -2,12 +2,21 @@
 
 import { createEnv } from '@t3-oss/env-core'
 import { onInvalidAccess, onValidationError } from './utils'
+import { z } from 'zod'
 
 export const clientEnv = createEnv({
-  clientPrefix: 'PUBLIC_',
-  client: {},
+  clientPrefix: 'NEXT_PUBLIC_',
+  client: {
+    NEXT_PUBLIC_MAX_FILE_SIZE: z.coerce.number().int().min(1),
+    NEXT_PUBLIC_ALLOWED_FILE_TYPES: z.string().min(1).transform((s) => s.split(",").map((s) => s.trim()).filter(Boolean)).pipe(z.array(z.string().min(1))),
+  },
   emptyStringAsUndefined: true,
-  runtimeEnv: process.env,
+  isServer: false,
+  skipValidation: true,
+  runtimeEnv: {
+    NEXT_PUBLIC_MAX_FILE_SIZE: process.env.NEXT_PUBLIC_MAX_FILE_SIZE,
+    NEXT_PUBLIC_ALLOWED_FILE_TYPES: process.env.NEXT_PUBLIC_ALLOWED_FILE_TYPES,
+  },
   onValidationError,
   onInvalidAccess,
 })
