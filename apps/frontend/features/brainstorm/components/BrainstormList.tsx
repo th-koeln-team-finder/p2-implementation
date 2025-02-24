54328c1ecd10b06db78c643b4578b1c975ff37da
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-02-24 15:31:31 +0100

    chore: improve breakpoints

--- apps/frontend/features/brainstorm/components/BrainstormList.tsx
+++ apps/frontend/features/brainstorm/components/BrainstormList.tsx
@@ -32,7 +32,7 @@ export async function BrainstormList({
       <BrainstormFilterBar />
       <Masonry
         masonryGutter="16px"
-        columnsCountBreakPoints={{ 350: 1, 640: 2, 768: 3, 1200: 4 }}
+        columnsCountBreakPoints={{ 350: 1, 640: 2, 768: 3, 1400: 4 }}
       >
         {brainstorms.map((brainstorm) => (
           <BrainstormListEntry key={brainstorm.id} brainstorm={brainstorm} />
