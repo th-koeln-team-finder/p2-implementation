82d4a8d233aa5aef14c613acd90456881e84f9b7
Leander <leander.gerwing@gmail.com>
2025-03-04 17:47:28 +0100

    feat: use "AvatarFallback" if image loading failed, use dicebear image when no profile pic is set

--- apps/frontend/features/auth/components/UserAvatar.tsx
+++ apps/frontend/features/auth/components/UserAvatar.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { getPublicFileUrl } from '@/features/file-upload/file-upload.actions'
-import type { UserWithImage } from '@/features/users/users.types'
-import { Avatar, AvatarImage } from '@repo/design-system/components/ui/avatar'
-import { cn } from '@repo/design-system/lib/utils'
-import { LoaderCircleIcon } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import {getPublicFileUrl} from '@/features/file-upload/file-upload.actions'
+import type {UserWithImage} from '@/features/users/users.types'
+import {Avatar, AvatarFallback, AvatarImage} from '@repo/design-system/components/ui/avatar'
+import {cn} from '@repo/design-system/lib/utils'
+import {useEffect, useState} from 'react'
 
 type UserAvatarProps = {
   user?: UserWithImage
@@ -14,12 +13,13 @@ type UserAvatarProps = {
 }
 
 export function UserAvatar({
-  user,
-  className,
-  fallbackClassName,
-}: UserAvatarProps) {
+                             user,
+                             className,
+                             fallbackClassName,
+                           }: UserAvatarProps) {
   const fallback = user ? user.name.slice(0, 2).toUpperCase() : 'AN'
   const [userAvatarUrl, setUserAvatarUrl] = useState<string | null>()
+  const [useFallback, setUseFallback] = useState(false)
 
   useEffect(() => {
     if (user?.image?.bucketPath) {
@@ -37,18 +37,17 @@ export function UserAvatar({
           alt={user?.name ?? user?.email ?? fallback}
         />
       )}
-      {!userAvatarUrl && !!user?.image?.bucketPath && (
-        <LoaderCircleIcon className="h-full w-full animate-spin" />
+      {!userAvatarUrl && (
+        <AvatarFallback className={cn('text-sm', fallbackClassName)}>
+          {fallback}
+        </AvatarFallback>
       )}
-      {!userAvatarUrl && !user?.image?.bucketPath && (
-        <AvatarImage
-          src={`https://api.dicebear.com/9.x/thumbs/svg?seed=${user?.name ?? 'anonymous'}&backgroundColor=ffdfbf,ffd5dc,d1d4f9,c0aede,b6e3f4,f88c49,f1f4dc,69d2e7&backgroundType=gradientLinear,solid&backgroundRotation=0,180,270,360&scale=80`}
-          alt={user?.name ?? user?.email ?? fallback}
-        />
-      )}
-      {/*<AvatarFallback className={cn('text-sm', fallbackClassName)}>
-        {fallback}
-      </AvatarFallback>*/}
+      {!user?.image?.bucketPath &&
+          <AvatarImage
+              src={`https://api.dicebear.com/9.x/thumbs/svg?seed=${user?.name ?? 'anonymous'}&backgroundColor=ffdfbf,ffd5dc,d1d4f9,c0aede,b6e3f4,f88c49,f1f4dc,69d2e7&backgroundType=gradientLinear,solid&backgroundRotation=0,180,270,360&scale=80`}
+              alt={user?.name ?? user?.email ?? fallback}
+          />
+      }
     </Avatar>
   )
 }
