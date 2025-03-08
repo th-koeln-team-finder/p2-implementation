49cd18b8f15bacb792c15cda8e4fe674adaca0c0
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-08 09:49:52 +0100

    fix: select statement for user skills

--- apps/frontend/features/userSkills/userSkills.query.ts
+++ apps/frontend/features/userSkills/userSkills.query.ts
@@ -6,44 +6,27 @@ export const getUserSkills = async (userId: string) => {
   const session = await authMiddleware()
   const loggedInUserId = session?.user?.id
 
-  /*const userSkills = await db.query.userSkills.findMany({
+  const isVerified = loggedInUserId
+    ? sql<boolean>`EXISTS (
+      SELECT 1 FROM ${Schema.userSkillVerification} verifications
+      WHERE verifications."userSkillId" = "userSkills"."id" AND verifications."userId" = ${loggedInUserId}
+    )`
+    : sql<boolean>`false`
+  const verificationCount = sql<number>`(
+      SELECT COUNT(*) FROM ${Schema.userSkillVerification} verifications
+      WHERE verifications."userSkillId" = "userSkills"."id"
+    )`.as('verificationCount')
+
+  return db.query.userSkills.findMany({
+    extras: {
+      isVerified: isVerified.as('isVerified'),
+      verificationCount,
+    },
     where: eq(Schema.userSkills.userId, userId),
     with: {
       skill: true,
-      userSkillVerification: {
-        columns: {},
-        extras: {
-          isVerified: eq(userSkillVerification.verifierId, loggedInUserId)
-        }
-      },
+      userSkillVerification: true,
     },
-    orderBy: [
-      desc(Schema.userSkills.level),
-      asc(Schema.userSkills.createdAt),
-    ],
-  })*/
-  const userSkills = await db
-    .select({
-      id: Schema.userSkills.id,
-      userId: Schema.userSkills.userId,
-      skillId: Schema.userSkills.skillId,
-      level: Schema.userSkills.level,
-      createdAt: Schema.userSkills.createdAt,
-      skill: Schema.skills.skill,
-      isVerified: sql<boolean>`EXISTS (
-      SELECT 1 FROM ${Schema.userSkillVerification} 
-      WHERE ${eq(Schema.userSkillVerification.userSkillId, Schema.userSkills.id)} 
-      AND ${eq(Schema.userSkillVerification.verifierId, loggedInUserId ?? '')}
-    )`.as('isVerified'),
-      verificationCount: sql<number>`(
-      SELECT COUNT(*) FROM ${Schema.userSkillVerification}
-      WHERE ${eq(Schema.userSkillVerification.userSkillId, Schema.userSkills.id)}
-    )`.as('verificationCount'),
-    })
-    .from(Schema.userSkills)
-    .leftJoin(Schema.skills, eq(Schema.skills.id, Schema.userSkills.skillId))
-    .where(eq(Schema.userSkills.userId, userId))
-    .orderBy(desc(Schema.userSkills.level), asc(Schema.userSkills.createdAt))
-
-  return userSkills
+    orderBy: [desc(Schema.userSkills.level), asc(Schema.userSkills.createdAt)],
+  })
 }
