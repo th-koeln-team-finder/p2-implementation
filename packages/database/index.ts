01a13f6bdcc9283bf7ade32b56bf2cc774dfb72b
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-10 19:28:04 +0100

    chore: remove db logs again

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -12,7 +12,7 @@ export const db = drizzle({
   connection: {
     connectionString: serverEnv.DATABASE_URL,
   },
-  logger: true,
+  logger: false,
 })
 
 export const AuthDrizzleAdapter = DrizzleAdapter(db, {
