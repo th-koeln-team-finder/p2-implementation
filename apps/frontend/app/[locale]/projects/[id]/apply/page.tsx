4d6cc04993c8ccd3abcb89c278576a520bc920e1
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-13 10:43:26 +0100

    feat: disallow application screen visit when not permitted

--- apps/frontend/app/[locale]/projects/[id]/apply/page.tsx
+++ apps/frontend/app/[locale]/projects/[id]/apply/page.tsx
@@ -1,4 +1,7 @@
 import ApplicationDetail from '@/features/Application/ApplicationDetails'
+import { getLocale } from 'next-intl/server'
+import { redirect } from '@/features/i18n/routing'
+import { hasSessionPermission } from '@/features/auth/auth.utils'
 
 export default async function Application({
   params,
@@ -6,6 +9,18 @@ export default async function Application({
   params: Promise<{ id: string }>
 }) {
   const { id } = await params
+  const locale = await getLocale()
+
+  const canCreateApplication = await hasSessionPermission(
+    'applyProject',
+    'create',
+  )
+  if (!canCreateApplication) {
+    return redirect({
+      locale,
+      href: `/projects/${id}`,
+    })
+  }
 
   return (
     <div className="container mx-auto px-4">
