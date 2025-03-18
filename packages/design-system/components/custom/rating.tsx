d79170dd3ca4f1fceb6a3a40f19569c61d1c77cb
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-18 20:37:31 +0100

    chore: fix imports for build

--- packages/design-system/components/custom/rating.tsx
+++ packages/design-system/components/custom/rating.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { cn } from '@/lib/utils'
+import { cn } from '../../lib/utils'
 import { useFieldContext } from '@formsignals/form-react'
 import { Star } from 'lucide-react'
 import React, { useState } from 'react'
