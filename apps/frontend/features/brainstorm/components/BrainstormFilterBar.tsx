3139d6d272755c3f678c842dbe9705b2092ed3b7
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-18 21:03:02 +0100

    chore: remove brainstorm pin bookmarks button

--- apps/frontend/features/brainstorm/components/BrainstormFilterBar.tsx
+++ apps/frontend/features/brainstorm/components/BrainstormFilterBar.tsx
@@ -1,13 +1,10 @@
 'use client'
-
-import { CanUserClient } from '@/features/auth/components/CanUser.client'
 import { revalidateBrainstorms } from '@/features/brainstorm/brainstorm.actions'
 import { useDebounceFunction } from '@/features/general/utils.hooks'
 import { useSignals } from '@preact/signals-react/runtime'
 import { Input } from '@repo/design-system/components/ui/input'
-import { Toggle } from '@repo/design-system/components/ui/toggle'
 import { cn } from '@repo/design-system/lib/utils'
-import { BookmarkIcon, InfoIcon, Loader2Icon, SearchIcon } from 'lucide-react'
+import { InfoIcon, Loader2Icon, SearchIcon } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import { useQueryState } from 'nuqs'
 import { useState } from 'react'
@@ -51,20 +48,6 @@ export function BrainstormFilterBar() {
             }}
           />
         </div>
-        <CanUserClient target="commentBrainstorm" action="create">
-          <Toggle
-            size="lg"
-            className="group ml-auto md:ml-0"
-            onClick={async () => {
-              await setBookmarks(bookmarksPinned ? null : 'pinned')
-              await revalidateBrainstorms()
-            }}
-            pressed={bookmarksPinned}
-          >
-            <BookmarkIcon className="group-data-[state=on]:fill-foreground" />
-            {translate('actionShowBookmarks')}
-          </Toggle>
-        </CanUserClient>
       </div>
       <div className="mt-2 flex flex-row items-center gap-2 rounded border-primary border-l-4 bg-primary/20 p-2 text-foreground text-sm">
         <InfoIcon className="size-4 min-w-4" />
