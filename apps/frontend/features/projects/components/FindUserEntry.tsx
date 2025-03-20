7bd63494bb380056719b8d679b2e9bb9d534e1f8
Joshua Gawenda <joshua.gawenda@gmail.com>
2025-03-20 11:32:39 +0100

    feat: add infinite loading + recommendations to users

--- apps/frontend/features/projects/components/FindUserEntry.tsx
+++ apps/frontend/features/projects/components/FindUserEntry.tsx
@@ -1,46 +1,110 @@
-import {WysiwygRenderer} from '@repo/design-system/components/WysiwygEditor/WysiwygRenderer'
-import {Card, CardContent, CardDescription, CardTitle,} from '@repo/design-system/components/ui/card'
-import {Button} from "@repo/design-system/components/ui/button";
-import {UserRoundPlus} from "lucide-react";
-import {UserWithImage} from "@/features/users/users.types";
-import {Avatar} from "@repo/design-system/components/ui/avatar";
-import {UserAvatar} from "@/features/auth/components/UserAvatar";
-import Link from "next/link";
+import { WysiwygRenderer } from '@repo/design-system/components/WysiwygEditor/WysiwygRenderer'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from '@repo/design-system/components/ui/card'
+import { Button } from '@repo/design-system/components/ui/button'
+import { ShellIcon, UserRoundPlus } from 'lucide-react'
+import { UserAvatar } from '@/features/auth/components/UserAvatar'
+import Link from 'next/link'
+import type { getUsers } from '@/features/users/users.query'
+import { getTranslations } from 'next-intl/server'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@repo/design-system/components/ui/tooltip'
 
+type FindSomeoneListEntryProps = {
+  user: Awaited<ReturnType<typeof getUsers>>[number]
+  projectId: string
+}
 
-export function FindSomeoneListEntry({user,projectId}: { user: UserWithImage,projectId:string }) {
+export async function FindSomeoneListEntry({
+  user,
+  projectId,
+}: FindSomeoneListEntryProps) {
+  const matchingTranslate = await getTranslations('projects.matching')
+  return (
+    <Link href={`/profile/${user.id}`}>
+      <Card className="relative h-full">
+        {user.totalMatchScore && +user.totalMatchScore > 0 && (
+          <TooltipProvider>
+            <Tooltip delayDuration={100}>
+              <TooltipTrigger className="absolute top-1 left-1 z-10 flex flex-row items-center gap-1 rounded bg-muted px-2 py-1 text-foreground text-xs">
+                <ShellIcon className="size-3" />
+                {(user.totalMatchScore * 100).toFixed(0)}
+              </TooltipTrigger>
+              <TooltipContent>
+                <h6 className="font-semibold text-lg">
+                  {matchingTranslate('tooltipTitle')}
+                </h6>
+                <p className="mb-2 max-w-xs text-muted-foreground">
+                  {matchingTranslate('tooltipDescription')}
+                </p>
+                <table className="text-left" cellSpacing="0">
+                  <tbody>
+                    <tr className="bg-card">
+                      <th className="p-1">
+                        {matchingTranslate('skillMatchingScore')}
+                      </th>
+                      <td className="min-w-12 p-1 text-right">
+                        {(user.skillMatchScore * 100).toFixed(0)}
+                      </td>
+                    </tr>
+                    <tr className="bg-card/40">
+                      <th className="p-1">
+                        {matchingTranslate('interestMatchingScore')}
+                      </th>
+                      <td className="min-w-12 p-1 text-right">
+                        {(user.projectTagMatchScore * 100).toFixed(0)}
+                      </td>
+                    </tr>
+                    <tr className="border-border border-t bg-card">
+                      <th className="p-1">
+                        {matchingTranslate('totalMatchingScore')}
+                      </th>
+                      <td className="min-w-12 p-1 text-right">
+                        {(user.totalMatchScore * 100).toFixed(0)}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
 
-    return (
-        <Link href={`/profile/${user.id}`}>
-            <Card className="h-full">
-                <CardContent className="flex flex-col items-center relative justify-between p-4 gap-2">
-                    <Link href={`/projects/${projectId}/invite`}>
-                     <Button variant={"ghost"} className="right-2 top-2 absolute">
-                        <UserRoundPlus className="size-5"/>
-                    </Button>
-                    </Link>
-                    <div className={"flex flex-row w-full justify-between gap-4 "}>
-                        <UserAvatar user={user} className="w-20 h-20"/>
-                        <div className="space-y-2 w-full">
-                            <div className="flex flex-row items-center justify-between gap-2">
-                                <p className={"text-sm"}>{user.occupation ?? " "}</p>
-                            </div>
-                            <div className="flex flex-row items-center justify-between gap-2">
-                                <CardTitle className="text-l">{user.name}</CardTitle>
-                            </div>
+        <CardContent className="relative flex flex-col items-center justify-between gap-2 p-4">
+          <Link href={`/projects/${projectId}/invite`}>
+            <Button className="absolute top-2 right-2" variant="ghost">
+              <UserRoundPlus className="size-5" />
+            </Button>
+          </Link>
+          <div className="flex w-full flex-row justify-between gap-4 ">
+            <UserAvatar user={user} className="h-20 w-20" />
+            <div className="w-full space-y-2">
+              <div className="flex flex-row items-center justify-between gap-2">
+                <p className={'text-sm'}>{user.occupation ?? ' '}</p>
+              </div>
+              <div className="flex flex-row items-center justify-between gap-2">
+                <CardTitle className="text-l">{user.name}</CardTitle>
+              </div>
 
-                            <CardDescription className="max-h-13 line-clamp-3 overflow-hidden">
-                                {user.bio && (
-                                    <WysiwygRenderer value={user.bio} renderAsString/>
-                                )}
-                            </CardDescription>
-                        </div>
-
-                    </div>
-                </CardContent>
-            </Card>
-        </Link>
-    )
+              <CardDescription className="line-clamp-3 max-h-13 overflow-hidden">
+                {user.bio && (
+                  <WysiwygRenderer value={user.bio} renderAsString />
+                )}
+              </CardDescription>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  )
 }
 
 /*
